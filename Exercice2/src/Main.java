239aa87f3ea75dbf13021993230c6fee7689ca25
Coulibaly <ibrahima.coulibaly@le-campus-numerique.fr>
2024-01-15 14:28:07 +0100

    exo2 commit 2

--- Exercice2/src/Main.java
+++ Exercice2/src/Main.java
@@ -2,19 +2,33 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
 
-    public static double plusPetitCarre(int n){
+    public static double plusPetitCarreSupN(int n){
         double result = 0;
         for(int i = 0; i < n; i++){
-            if(Math.sqrt(n)%2 == 0){
+            if(n%Math.sqrt(n) == 0){
                 result = Math.sqrt(n);
             }else{
                 n++;
             }
         }
-        return result;
+        return result*result;
+    }
+
+    public static double plusPetitCarreInfN(int n){
+
+        double result = 0;
+        for(int i = n; i > 0; i--){
+            if(n%Math.sqrt(n) == 0){
+                result = Math.sqrt(n);
+            }else{
+                n--;
+            }
+        }
+        return result*result;
     }
     public static void main(String[] args) {
-        System.out.println(plusPetitCarre(30));
+        System.out.println(plusPetitCarreSupN(11));
+        System.out.println(plusPetitCarreInfN(40));
 
 
     }
