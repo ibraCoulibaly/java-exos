b554be7d9d1c83619a000cf68ff1afe4ed0fdc31
Coulibaly <ibrahima.coulibaly@le-campus-numerique.fr>
2024-01-16 14:22:18 +0100

    exo2 modif

--- Exercice2/src/Main.java
+++ Exercice2/src/Main.java
@@ -3,33 +3,35 @@
 public class Main {
 
     public static double plusPetitCarreSupN(int n){
-        double result = 0;
-        for(int i = 0; i < n; i++){
-            if(n%Math.sqrt(n) == 0){
-                result = Math.sqrt(n);
-            }else{
-                n++;
-            }
+
+        while(n % Math.sqrt(n) != 0){
+            n++;
         }
-        return result*result;
+        return n;
     }
 
     public static double plusPetitCarreInfN(int n){
 
-        double result = 0;
-        for(int i = n; i > 0; i--){
-            if(n%Math.sqrt(n) == 0){
-                result = Math.sqrt(n);
-            }else{
-                n--;
-            }
+        while(n % Math.sqrt(n) != 0){
+            n--;
         }
-        return result*result;
+        return n;
     }
-    public static void main(String[] args) {
-        System.out.println(plusPetitCarreSupN(11));
-        System.out.println(plusPetitCarreInfN(40));
 
+//        double result = 0;
+//        for(int i = n; i > 0; i--){
+//            if(n%Math.sqrt(n) == 0){
+//                result = Math.sqrt(n);
+//            }else{
+//                n--;
+//            }
+//        }
+//        return result*result;
+//    }
+    public static void main(String[] args) {
+        System.out.println(plusPetitCarreSupN(59));
+        System.out.println(plusPetitCarreSupN(64));
+        System.out.println(plusPetitCarreInfN(63));
 
     }
 }
